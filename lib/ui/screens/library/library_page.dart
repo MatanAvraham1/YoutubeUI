b7f28ca784d9f6401471504248d0a89adba7f9c3
Matan <matan03123321@gmail.com>
2021-05-07 15:52:55 +0300

    added static data

--- lib/ui/screens/library/library_page.dart
+++ lib/ui/screens/library/library_page.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:random_color/random_color.dart';
 import 'package:youtube/constants/db.dart';
 import 'package:youtube/translations/locale_keys.g.dart';
 import 'package:easy_localization/easy_localization.dart';
 
 class LibraryPage extends StatelessWidget {
-  final RandomColor _randomColor = RandomColor();
-
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
